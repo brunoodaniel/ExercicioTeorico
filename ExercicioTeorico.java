716d11e4d005eedd12ddd8422eded693c3c068b9
Bruno Daniel <104467144+brunoodaniel@users.noreply.github.com>
2023-10-01 19:51:05 -0300

    Exercício teórico SOLID

---
+++ ExercicioTeorico.java
@@ -0,0 +1,43 @@
+package ExercicioTeorico;
+
+public class ExercicioTeorico {
+    /*
+
+1. Qual princípio SOLID se concentra na ideia de que as classes devem ter apenas um motivo para mudar?
+   b) Princípio da Responsabilidade Única (SRP)
+
+2. Qual princípio SOLID diz que "as classes derivadas devem ser substituíveis por suas classes base"?
+   a) Princípio da Substituição de Liskov (LSP)
+
+3. O Princípio da Responsabilidade Única (SRP) afirma que uma classe deve ter apenas uma razão para mudar. Qual é o problema de uma classe que viola esse princípio?
+   a) Baixa coesão
+
+4. Qual princípio SOLID sugere que as classes devem estar abertas para extensão, mas fechadas para modificação?
+   c) Princípio do Aberto-Fechado (OCP)
+
+5. O Princípio do Aberto-Fechado (OCP) incentiva o uso de quais conceitos de programação orientada a objetos?
+   b) Encapsulamento e composição
+
+6. Qual princípio SOLID se concentra na ideia de que as classes de alto nível não devem depender de classes de baixo nível, mas sim de abstrações?
+   d) Princípio da Inversão de Dependência (DIP)
+
+7. O Princípio da Inversão de Dependência (DIP) sugere o uso de quais conceitos para reduzir o acoplamento entre módulos?
+   a) Interfaces e classes abstratas
+
+8. Qual dos princípios SOLID enfatiza que as classes devem ter uma única responsabilidade, ou seja, fazer apenas uma coisa e fazê-la bem?
+   b) Princípio da Responsabilidade Única (SRP)
+
+9. O Princípio da Substituição de Liskov (LSP) é um dos princípios SOLID. Qual é sua principal implicação para as classes derivadas?
+   c) Elas devem poder substituir os objetos da classe base sem quebrar a corretude do programa.
+
+10.Qual dos princípios SOLID é frequentemente aplicado em conjunto com o Padrão de Fábrica (Factory Pattern) para criar objetos sem conhecer as classes concretas?
+   d) Princípio da Inversão de Dependência (DIP)
+
+11.Considere a seguinte afirmação: "O Princípio da Substituição de Liskov (LSP) é uma extensão do Princípio do Aberto-Fechado (OCP)". Essa afirmação é:
+   a) Verdadeira
+
+12.Qual dos seguintes cenários NÃO é uma violação do Princípio da Responsabilidade Única (SRP)?
+   b) Uma classe que lê dados de um arquivo e os armazena em um banco de dados.
+
+    */
+}
